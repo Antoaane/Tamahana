64d84a86d16ed3c3db5f660ab78eba587a047e64
Antoine Lauzis <antoinelauzis@gmail.com>
2026-04-24 01:04:36 +0200

    chore(db): add initial payload migrations

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260423_230153 from './20260423_230153';
+
+export const migrations = [
+  {
+    up: migration_20260423_230153.up,
+    down: migration_20260423_230153.down,
+    name: '20260423_230153'
+  },
+];
